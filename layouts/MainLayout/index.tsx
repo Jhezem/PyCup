8987345e919638da4cdb26a70e6f4d346bc43db5
Jhezem <mmartinhez08@gmail.com>
2023-02-12 21:07:40 -0600

    add main layout component

---
+++ layouts/MainLayout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../../components/Header";
+import styles from "./MainLayout.module.css";
+
+export default function MainLayout({children} : React.PropsWithChildren) {
+  return (
+    <div className={styles.container}>
+    <Header />
+    {children}
+    </div>
+  )
+}
