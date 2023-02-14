84c48088cdb79e1e5b91f69fe5c94e11420a3551
Jhezem <mmartinhez08@gmail.com>
2023-02-13 18:44:31 -0600

    add footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+        <h4>Jairo Py Cup 2023</h4>
+    </footer>
+  )
+}
