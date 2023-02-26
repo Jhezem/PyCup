95d51abf690aba915ad71a134a4e85df77a31fde
Jhezem <mmartinhez08@gmail.com>
2023-02-26 12:32:44 -0600

    add cta section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import MainLayout from "../layouts/MainLayout";
-import styles from "../styles/Home.module.css";
+import { TwoColumn } from "../shared/Cta/TwoColumn";
 
 export default function Home() {
   return (
@@ -10,8 +10,22 @@ export default function Home() {
         <meta name="description" content="Py Cup sitio web oficial" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}></main>
+      <TwoColumn
+        subtitle="Solo los verdaderos improvisan."
+        title="Freestyle Network"
+        description="Entra a la nueva e innovadora red social de freestylers, comparte
+              tu contenido, y ganate un puesto entre los mejores."
+        buttonText="Registrate"
+        img={{
+          src: "/img/amongus.png",
+          width: 712,
+          height: 640,
+          alt: "Rapper",
+        }}
+        imgStyles={{
+          transform: "translateY(-15%)",
+        }}
+      />
     </MainLayout>
   );
 }
