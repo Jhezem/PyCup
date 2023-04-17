c08983822e9b7bbbe921eecd61f19ef81b8d90c6
Jhezem <mmartinhez08@gmail.com>
2023-04-16 23:06:53 -0600

    add auth provider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "../hooks/useAuth";
 import "../styles/globals.css";
 
 export default function RootLayout({
@@ -8,7 +9,9 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <AuthProvider>
+        <body>{children}</body>
+      </AuthProvider>
     </html>
   );
 }
